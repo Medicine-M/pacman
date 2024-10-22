1cf880e455dcb3431d25638905690028cc4e643e
Medicine <mdyouhou3@gmail.com>
2024-10-22 16:56:05 +0900

    doctestを追加しました

--- display.py
+++ display.py
@@ -1,11 +1,34 @@
-# from field import Field
+from player import Player
 
 
-# class Display:
-#     """
-#     Displayクラス
-#     Displayクラスは、画面にフィールドを表示します。
-#     """
+class Display:
+    """
+    Displayクラス
+    Displayクラスは、画面にフィールドを表示します。
+    """
 
-#     def __init__(self) -> None:
-#         pass
+    def __init__(self) -> None:
+        """
+        Displayクラスの初期化を行う関数
+
+        """
+        pass
+
+    def update(self, field_data: dict[str, list[Player]]) -> None:
+        """
+        画面表示の更新をする
+
+        Returns:
+            None
+
+        Examples:
+            >>> field = Display()
+            >>> field.update()
+            >>> ""
+        """
+        pass
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
